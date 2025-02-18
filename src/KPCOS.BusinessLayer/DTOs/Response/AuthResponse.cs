116a181a2f192b107373e2225dc59df15d7de7d1
sa160148 <tongtbsa160148@fpt.edu.vn>
2025-02-18 10:58:29 +0700

    fix(auth): change response for login

--- src/KPCOS.BusinessLayer/DTOs/Response/AuthResponse.cs
+++ src/KPCOS.BusinessLayer/DTOs/Response/AuthResponse.cs
@@ -2,11 +2,11 @@
 
 public class AuthResponse
 {
-    
+
 }
 
 public class SigninResponse
 {
     public String Token { get; set; }
-  
+    public UserResponse User { get; set; } /*= new UserResponse();*/
 }
---
+++ src/KPCOS.BusinessLayer/DTOs/Response/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KPCOS.BusinessLayer.DTOs.Response;
+
+public class UserResponse
+{
+    public string? FullName { get; set; }
+    public string? Avatar { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Password { get; set; }
+}
