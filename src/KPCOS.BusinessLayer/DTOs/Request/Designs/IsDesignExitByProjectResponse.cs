406ac5a875fcb509b42726b70ba7a53774ea1b11
sa160148 <tongtbsa160148@fpt.edu.vn>
2025-03-09 04:14:14 +0700

    chore(project-design): add function check is any design with 3d and confirm exited

---
+++ src/KPCOS.BusinessLayer/DTOs/Request/Designs/IsDesignExitByProjectResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KPCOS.BusinessLayer.DTOs.Request.Designs;
+
+public class IsDesignExitByProjectResponse
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? IsExit3DConfirmed { get; set; }
+}
