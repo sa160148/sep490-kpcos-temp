7bfd97fdf2c8f0ae020859999af969c222afed4a
sa160148 <tongtbsa160148@fpt.edu.vn>
2025-03-19 09:33:30 +0700

    chore(entity): change some field and add more entities

---
+++ src/KPCOS.DataAccessLayer/Entities/IssueType.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace KPCOS.DataAccessLayer.Entities;
+
+public partial class IssueType
+{
+    public Guid Id { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+        public bool? IsActive { get; set; }
+    public string Name { get; set; } = null!;
+    public virtual ICollection<ProjectIssue> ProjectIssues { get; set; } = new List<ProjectIssue>();
+}
